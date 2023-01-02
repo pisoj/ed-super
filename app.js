198bcf126052164831d07e71cd801b87da895357
Josip <87895700+pisoj@users.noreply.github.com>
2023-01-02 12:44:13 +0100

    css enhanced

--- app.js
+++ app.js
@@ -25,26 +25,13 @@ function calculateAverage(numbers) {
 function addAverage(average) {
     const color = (average - 1) * 30;
 
-    const head = document.getElementsByTagName("head")[0];
-    const style = document.createElement("style");
-    const text = document.createTextNode(`
-    .grades-average {
-        background: hsl(${color}, 71%, 90%);
-    }
-    .table-end {
-        border-bottom-left-radius: 10px;
-        border-bottom-right-radius: 10px;
-    }
-    `);
-    style.appendChild(text);
-    head.appendChild(style);
-
     const gradesTable = document.getElementsByClassName("grades-table")[0];
     const row = document.createElement("div");
-    const rowClasses = ["flex-table", "row", "grades-average", "table-end"];
+    const rowClasses = ["flex-table", "row", "table-end"];
     for(let i = 0; i < rowClasses.length; i++) {
         row.classList.add(rowClasses[i]);
     };
+    row.setAttribute("style", `background: hsl(${color}, 71%, 90%);`);
 
     const titleColumn = document.createElement("div");
     titleColumn.classList.add("flex-row");
