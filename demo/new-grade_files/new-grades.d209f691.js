c3a25f28f8712021d54415c6da5c9e73b780b796
pisoj <josip.tismat@gmail.com>
2023-01-13 20:49:33 +0100

    Final demo

--- demo/new-grade_files/new-grades.d209f691.js
+++
@@ -1,4 +0,0 @@
-var tableContent = document.querySelectorAll('.new-grades-table');
-var dropdownMenu = document.querySelector('.js-dropdown-menu-content');
-
-var test = new DropdownMenuContentSwitch(tableContent, dropdownMenu);
